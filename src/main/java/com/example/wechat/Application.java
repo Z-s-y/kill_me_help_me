2f9f268fde7fa8c6a62053e550f7a90d478e125b
Z-s-y <1120946973@qq.com>
2026-02-21 23:16:23 +0800

    GAME_PROCESS-DAO逻辑 Test

--- src/main/java/com/example/wechat/Application.java
+++ src/main/java/com/example/wechat/Application.java
@@ -1,10 +1,15 @@
 package com.example.wechat;
 
+import com.example.wechat.dao.GameDao;
 import com.example.wechat.dao.UserDao;
+import com.example.wechat.dao.impl.GameDaoImpl;
 import com.example.wechat.dao.impl.UserDaoImpl;
+import com.example.wechat.model.Game;
 import com.example.wechat.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 /**
  * 微信小程序后端应用主类
@@ -19,7 +24,11 @@ public class Application {
         System.out.println("访问地址: http://localhost:5000");
         System.out.println("========================================\n");
 
-        UserDao userDao = new UserDaoImpl();
-        userDao.deleteById("1");
+        Date date = new Date("2028-01-01 00:00:00");
+        Date date2 = new Date("2028-01-02 00:00:00");
+
+        Game game = new Game(1, "type", date, date2, "master");
+        GameDao gameDao = new GameDaoImpl();
+        gameDao.insert(game);
     }
 }
